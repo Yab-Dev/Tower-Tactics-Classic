1a75a719a28e297409c7a5b9a33410b728805737
YabDev <ethan.yabsley@gmail.com>
2025-12-11 15:55:58 -0800

    Added win and lose screen. Buttons do not work

---
+++ Assets/Scripts/UI/EndingScreenUI.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingScreenUI : MonoBehaviour
+{
+    [Header("Prefabs")]
+    [SerializeField] private GameObject winScreenPrefab;
+    [SerializeField] private GameObject loseScreenPrefab;
+
+
+
+    private void Awake()
+    {
+        GameManager.OnGameWin += GameWin;
+        GameManager.OnGameLost += GameLose;
+    }
+
+    private void GameWin()
+    {
+        Instantiate(winScreenPrefab, transform);
+    }
+
+    private void GameLose()
+    {
+        Instantiate(loseScreenPrefab, transform);
+    }
+}
